13d95dc79144f7a439930ef0f8141e4f8235f449
Jacobo Vidal <jacobovidal@users.noreply.github.com>
2025-06-17 16:49:45 +0200

    Reorder cors code

--- api/index.ts
+++ api/index.ts
@@ -14,8 +14,8 @@ const app = express();
 
 app.set("trust proxy", true);
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
